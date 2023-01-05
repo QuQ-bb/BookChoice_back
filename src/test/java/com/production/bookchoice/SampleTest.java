c87bc767b2786d0f45136972075294d2ff545f49
QuQ-bb <quqsue@gmail.com>
2023-01-05 13:40:19 +0900

    setting

---
+++ src/test/java/com/production/bookchoice/SampleTest.java
@@ -0,0 +1,16 @@
+package com.production.bookchoice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
