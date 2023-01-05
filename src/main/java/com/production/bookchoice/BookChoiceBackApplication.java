babae2922770f0c7ca4433690fc3b8042da5a6be
QuQ-bb <quqsue@gmail.com>
2023-01-05 13:16:43 +0900

    setting

---
+++ src/main/java/com/production/bookchoice/BookChoiceBackApplication.java
@@ -0,0 +1,13 @@
+package com.production.bookchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookChoiceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookChoiceBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/production/bookchoice/BookChoiceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.production.bookchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookChoiceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
