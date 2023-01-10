e64327bae558c2b586c5cc925923a7c69c31dd35
QuQ-bb <quqsue@gmail.com>
2023-01-11 08:40:30 +0900

    검색/상품조회

--- src/main/java/com/production/bookchoice/book/BookController.java
+++ src/main/java/com/production/bookchoice/book/BookController.java
@@ -1,5 +1,6 @@
 package com.production.bookchoice.book;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,22 @@ public class BookController {
 	private BookService bookService;
 	
 	@GetMapping("bookList")
-	public String aladinList()throws Exception{
-		String bl = bookService.bookList();
+	public JSONObject bookList()throws Exception{
+		JSONObject bl = bookService.bookList();
 		
 		return bl;
+	}
+	
+	@GetMapping("bookDetail")
+	public JSONObject bookDetail(String itemId)throws Exception{
+		JSONObject bd = bookService.bookDetail(itemId);
+		
+		return bd;
+	}
+	@GetMapping("bookSearch")
+	public JSONObject bookSearch(String search)throws Exception{
+		JSONObject bs = bookService.bookSearch(search);
+		return bs;
 	}
 
 }
--- src/main/java/com/production/bookchoice/book/BookService.java
+++ src/main/java/com/production/bookchoice/book/BookService.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.core.JsonParser;
+
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
@@ -18,14 +20,14 @@ public class BookService {
 	@Value("${aladin.ttb.key}")
 	private String ttbKey;
 	
-	public String bookList()throws Exception{
+	public JSONObject bookList()throws Exception{
 		
 		WebClient webClient = WebClient.builder()
 									   .baseUrl("http://www.aladin.co.kr/ttb/api/ItemList.aspx")
 									   .build();
 		
 			Mono<ResponseEntity<String>> res = webClient.get()
-														.uri("?ttbkey="+ttbKey+"&QueryType=ItemEditorChoice&MaxResults=10&CategoryId=170&start=1&SearchTarget=Book&output=js&Version=20131101")
+														.uri("?ttbkey="+ttbKey+"&QueryType=ItemEditorChoice&MaxResults=50&CategoryId=170&start=1&SearchTarget=Book&output=js&Version=20131101")
 														.retrieve() //응답받게 해주는 부분
 														.toEntity(String.class);
 			
@@ -36,11 +38,59 @@ public class BookService {
 			Object obj = parser.parse(str);
 			
 			JSONObject jsonObject = (JSONObject)obj;
+			String i = String.valueOf(jsonObject.get("totalResults"));
 			
 			log.info("webclient => {}",jsonObject);
 			
 			
-			return str;
+			return jsonObject;
+	}
+	
+	public JSONObject bookDetail(String itemId)throws Exception{
+		
+		WebClient webClient = WebClient.builder()
+									   .baseUrl("http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx")
+									   .build();
+		
+		Mono<ResponseEntity<String>> res = webClient.get()
+													.uri("?ttbkey="+ttbKey+"&itemIdType=ISBN13&ItemId="+itemId+"&output=js&Version=20131101")
+													.retrieve()
+													.toEntity(String.class);
+		
+		String str = res.block().getBody();
+		
+		JSONParser parser = new JSONParser();
+		
+		Object obj = parser.parse(str);
+		
+		JSONObject jsonObject = (JSONObject)obj;
+		
+		return jsonObject;
+		
+		
+	}
+	public JSONObject bookSearch(String search)throws Exception{
+		
+		WebClient webClient = WebClient.builder()
+									   .baseUrl("http://www.aladin.co.kr/ttb/api/ItemSearch.aspx")
+									   .build();
+		
+		Mono<ResponseEntity<String>> res = webClient.get()
+													.uri("?ttbkey="+ttbKey+"&Query="+search+"&QueryType=Keyword&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101")
+													.retrieve()
+													.toEntity(String.class);
+		
+		String str = res.block().getBody();
+		
+		JSONParser parser = new JSONParser();
+		
+		Object obj = parser.parse(str);
+		
+		JSONObject jsonObject = (JSONObject)obj;
+		
+		return jsonObject;
+		
+		
 	}
 
 }
